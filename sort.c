9794adbc34f00f1a4a153578c6cda902b0a9651f
Camden Mac Leod <camden.macleod@pm.me>
2023-04-05 18:23:43 -0400

    Finish assignment

--- sort.c
+++ sort.c
@@ -1,20 +1,125 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
+
+/*
+ * Binary tree structure
+ * int arr[] = { 5, 10, 3, 2, 1 }
+ *       5
+ *      /  \
+ *    10    3
+ *   /  \
+ *  2    1
+ */
+
+// Used by heapSort()
+// Reorganizes the heap to put the largest one at the top
+// n is size
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Index of largest item
+    int left = (2 * i) + 1;
+    int right = (2 * i) + 2;
+
+    // Set largest
+    if (left < n && arr[left] > arr[largest]) // Left
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+    // Build our max heap (a heap that is organized and all subclasses are less than or equal to parent class
+    for (int i = (n / 2) - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Restart heapify
+        heapify(arr, i, 0);
+    }
 }
 
+// Used by mergeSort()
+void merge(int pData[], int l, int mid, int r) {
+    int firstHalf = mid - l + 1;
+    int secHalf = r - mid;
+
+    // Temporary arrays
+    int *left = malloc(firstHalf * sizeof(int));
+    int *right = malloc(secHalf * sizeof(int));
+
+    // Move values to temp arrays
+    for (int i = 0; i < firstHalf; i++)
+        left[i] = pData[i + l];
+    for (int i = 0; i < secHalf; i++)
+        right[i] = pData[mid + i + 1];
+
+    int leftIterator = 0;
+    int rightIterator = 0;
+    int mergeIterator = l;
+
+    // We compare and move values back to pData[]
+    while (leftIterator < firstHalf && rightIterator < secHalf) {
+        if (left[leftIterator] <= right[rightIterator]) {
+            pData[mergeIterator] = left[leftIterator];
+            leftIterator++;
+        }
+        else {
+            pData[mergeIterator] = right[rightIterator];
+            rightIterator++;
+        }
+
+        mergeIterator++;
+    }
+
+    // Empty any remaining values from left[] or right[] if present
+    while (leftIterator < firstHalf) {
+        pData[mergeIterator] = left[leftIterator];
+        leftIterator++;
+        mergeIterator++;
+    }
+    while (rightIterator < secHalf) {
+        pData[mergeIterator] = right[rightIterator];
+        rightIterator++;
+        mergeIterator++;
+    }
+
+    free(left);
+    free(right);
+}
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+    if (l < r) {
+        int mid = (l + r) / 2;
+
+        // Sort left and right
+        mergeSort(pData, l, mid);
+        mergeSort(pData, mid + 1, r);
+
+        merge(pData, l, mid, r);
+    }
 }
 
 // parses input file to an integer array
@@ -71,7 +176,10 @@ int main(void)
 	clock_t start, end;
 	int i;
     double cpu_time_used;
-	char* fileNames[] = { "input1.txt", "input2.txt", "input3.txt", "input4.txt" };
+	char* fileNames[] = { "input1.txt",
+                          "input2.txt",
+                          "input3.txt",
+                          "input4.txt" };
 	
 	for (i=0;i<4;++i)
 	{
